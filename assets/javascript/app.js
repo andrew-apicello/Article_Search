6083a1f203c6c043fe53e043e9577f387dfba59e
Andrew C. Apicello <andrew.apicello@gmail.com>
2017-11-13 13:53:23 -0500

    clear on new queries and add styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,46 @@
-var apiKey = "c7bdf21eb93e4d239856ca83974cc925"
-var query = "hurricane"
-// var beginDate = "20160511"
-// var endDate = "20160512"
 
 
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-	'api-key': apiKey,
-	'q': query,
-  // 'begin_date': beginDate,
-  // 'end_date': endDate
-});
+$("#submit").on("click", function() {
+	event.preventDefault();
+	$(".newArticles").empty();
+
+	var apiKey = "c7bdf21eb93e4d239856ca83974cc925"
+	var query = $("#query").val().trim();
+	var number = $("#number").val().trim();
+	console.log(query)
+	// var beginDate = $("#beginDate").val().trim();
+	// var endDate =  $("#endDate").val().trim();
+
+	// var beginDate= "";
+	// var endDate= "";
+
+	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	url += '?' + $.param({
+		'api-key': apiKey,
+		'q': query,
+		// 'begin_date': beginDate,
+		// 'end_date': endDate
+	});
 
 
-$.ajax({
-	url: url,
-	method: 'GET',
-}).done(function(result) {
+	$.ajax({
+		url: url,
+		method: 'GET',
+	}).done(function(result) {
 		console.log(result);
+		var number = $("#number").val().trim();
+		for (var i = 0; i < number; i++){
+			console.log(number);
+			// console.log(result.response.docs[i])
+			var article = result.response.docs[i]
+			var newDiv = $("<div><a target='_blank' href='"+ article.web_url +"'><h3>" + article.headline.main + "</h3></a><h5>" + article.byline.original+ "</h5><h5>" + article.pub_date.slice(0,10) + "</h5></div></br>");
+			$(".newArticles").append(newDiv);
+	}
 	}).fail(function(err) {
 		throw err;
+	});
+
+	
+
 });
